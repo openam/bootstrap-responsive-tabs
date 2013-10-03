f5c01b4e607d38305649b3d93d05bdc8197976b9
Michael Tuttle <openam@gmail.com>
2013-10-02 21:18:09 -0600

    added javascript for Bootstrap 2.3.2

---
+++ js/responsive-tabs-2.3.2.js
@@ -0,0 +1,64 @@
+if (typeof fakewaffle === 'undefined') {
+    var fakewaffle = {};
+}
+
+fakewaffle.responsiveTabs = function (collapseDisplayed) {
+    "use strict";
+    var tabGroups         = $('.nav-tabs.responsive'),
+        hidden            = '',
+        visible           = '';
+
+    if (typeof collapseDisplayed === 'undefined') {
+        collapseDisplayed = ['phone', 'tablet'];
+    }
+
+    $.each(collapseDisplayed, function () {
+        hidden  += ' hidden-' + this;
+        visible += ' visible-' + this;
+    });
+
+    $.each(tabGroups, function () {
+        var $tabGroup   = $(this),
+            tabs        = $tabGroup.find('li a'),
+            collapseDiv = $("<div></div>", {
+                "class" : "accordion responsive" + visible,
+                "id"    : 'collapse-' + $tabGroup.attr('id')
+            });
+
+        $.each(tabs, function () {
+            var $this  = $(this),
+                active = '';
+
+            if ($this.parent().hasClass('active')) {
+                active = ' in';
+            }
+
+            collapseDiv.append(
+                $('<div>').attr('class', 'accordion-group').html(
+                    $('<div>').attr('class', 'accordion-heading').html(
+                        $('<a>', {
+                            'class' : 'accordion-toggle',
+                            'data-toggle': 'collapse',
+                            'data-parent' : '#collapse-' + $tabGroup.attr('id'),
+                            'href' : '#collapse-' + $this.attr('href').replace(/#/g, ''),
+                            'text': $this.html()
+                        })
+                    )
+                ).append(
+                    $('<div>', {
+                        'id' : 'collapse-' + $this.attr('href').replace(/#/g, ''),
+                        'class' : 'accordion-body collapse' + active
+                    }).html(
+                        $('<div>').attr('class', 'accordion-inner').html(
+                            $('#' + $this.attr('href').replace(/#/g, '')).html()
+                        )
+                    )
+                )
+            );
+        });
+
+        $tabGroup.after(collapseDiv);
+        $tabGroup.addClass(hidden);
+        $('.tab-content.responsive').addClass(hidden);
+    });
+};
